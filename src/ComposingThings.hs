45b4b32760bb88fad7d83ff8f1158b8a5cd4870c
Alistair Burrowes <alistair.burrowes@gmail.com>
2019-08-27 20:27:28 +1000

    Working

--- src/ComposingThings.hs
+++ src/ComposingThings.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -7,8 +8,7 @@ module ComposingThings where
 
 import Control.Monad.IO.Class
 import Data.Functor.Identity
-
-
+-- import Control.Monad.Reader
 
 
 data User = User String
@@ -33,18 +33,18 @@ updateUserNameInDb' userId userName = do
 
 
 
-class Monad m => MonadReader r m | m -> r where
+class Monad m => MonadReader r m where
   ask :: m r
 
 
-maybeUpdateUserNameInDbAndLog :: (MonadReader String m, DBTalker m) => String -> String -> m ()
-maybeUpdateUserNameInDbAndLog = do
-    connectionString <- ask
-    db <- connectDb connectionString
-    user <- fetchUserFromDB' db userId
-    let updatedUser = setUserName userName user
-    result <- updateUserInDB' db userId updatedUser
-    pure result
+-- maybeUpdateUserNameInDbAndLog :: (MonadReader String m, DBTalker m) => String -> String -> m ()
+-- maybeUpdateUserNameInDbAndLog = do
+--     connectionString <- ask
+--     db <- connectDb connectionString
+--     user <- fetchUserFromDB' db userId
+--     let updatedUser = setUserName userName user
+--     result <- updateUserInDB' db userId updatedUser
+--     pure result
 
 
 
@@ -57,13 +57,17 @@ maybeUpdateUserNameInDbAndLog = do
 data ReaderIO env a = ReaderIO { runReaderIO :: env -> IO a }
 
 newtype Reader env a = Reader { runReader :: env -> a }
+  deriving (Functor)
 
-newtype MyRealApp' a = MyRealApp' { unMyRealApp :: Reader String (IO a) }
-  deriving (Functor, Applicative, Monad, MonadIO)
+newtype MyRealApp' a = MyRealApp' { unMyRealApp' :: Reader String (IO a) }
+  -- deriving (Functor, Applicative, Monad, MonadIO)
 
 -- instance DBTalker MyRealApp' where
 --   fetchUserFromDB' userId = liftIO $ reallyFetchFromDBInIO userId
 --   updateUserInDB' userId user = liftIO $ reallyUpdateDBInIO userId user
+
+instance Functor MyRealApp' where
+  fmap f (MyRealApp' (Reader envToIOA)) = undefined
 
 
 reallyFetchFromDBInIO :: String -> IO User
@@ -72,5 +76,68 @@ reallyFetchFromDBInIO = undefined
 reallyUpdateDBInIO :: String -> User -> IO ()
 reallyUpdateDBInIO = undefined
 
-instance MonadReader String MyRealApp' where
-  ask = MyRealApp' $ Reader $ \dbConn -> pure dbConn
+
+
+
+
+
+
+
+
+
+
+
+
+newtype Compose f g a = Compose { getCompose :: f (g a) }
+
+
+
+blah :: Compose Maybe Identity Int
+blah = Compose $ Just $ Identity 1
+
+
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) = undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure = undefined
+  (<*>) :: Compose f g (a -> b)
+        -> Compose f g a
+        -> Compose f g b
+  (Compose f) <*> (Compose a) = undefined
+
+
+
+
+
+
+
+
+
+
+instance (Monad f, Monad g) => Monad (Compose f g) where
+  return = pure
+  (>>=) :: Compose f g a
+        -> (a -> Compose f g b)
+        -> Compose f g b
+  (>>=) = undefined
+
+  -- join :: (Monad f, Monad g) => f (g (f (g a))) -> f (g a)
+  -- join = undefined
